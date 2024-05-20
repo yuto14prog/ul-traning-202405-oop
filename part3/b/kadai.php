daa8c166422b889a31f3d65cc0a0a85743979cda
yuto14prog <yuto14prog@gmail.com>
2024-05-20 18:59:08 +0900

    step3

---
+++ part3/b/kadai.php
@@ -0,0 +1,70 @@
+<?php
+class Book
+{
+    private $title;
+    private $page_size;
+    public function __construct($title, $page_size)
+    {
+        $this->title = $title;
+        $this->page_size = $page_size;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function setTitle($value)
+    {
+        $this->title = $value;
+    }
+    public function getPageSize()
+    {
+        return $this->page_size;
+    }
+    public function setPageSize($value)
+    {
+        $this->page_size = $value;
+    }
+}
+
+
+class Bookshelf
+{
+    protected $books = [];
+    public function addBook($book)
+    {
+        if (!$this->canAddBook($book)) return false;
+        array_push($this->books, $book);
+        return true;
+    }
+    public function canAddBook($book)
+    {
+        return true;
+    }
+    public function findBookByTitle($title)
+    {
+        foreach ($this->books as $book) {
+            if ($book->getTitle() === $title) {
+                echo $title . "はあります" . "\n";
+                return $book;
+            }
+        }
+        echo $title . "はないです" . "\n";
+        return null;
+    }
+}
+
+class ThinBookshelf extends Bookshelf
+{
+    public function canAddBook($book)
+    {
+        return $book->getPageSize() < 20;
+    }
+}
+
+$bookshelf = new ThinBookshelf;
+$bookshelf->addBook(new Book("坊ちゃん", 20));
+$bookshelf->findBookByTitle("坊ちゃん"); // 坊ちゃんはないです
+
+$bookshelf = new ThinBookshelf;
+$bookshelf->addBook(new Book("こころ", 5));
+$bookshelf->findBookByTitle("こころ"); // こころはあります
